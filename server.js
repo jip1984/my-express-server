1fd22d508a429392496f392bbf3272ad5cc1cf0a
jon <jonprosser84@gmail.com>
2021-05-03 16:04:24 +0100

    added a post request

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", (req, res) => {
+    res.send('Thanks for posting that');
+})
+
 app.listen(3000, (req, res) => {
     console.log('Server started on port 3000');
 });
