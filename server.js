b12e4205b61f37d7902e2bb603996e3a25df3b56
jon <jonprosser84@gmail.com>
2021-05-03 16:18:39 +0100

    fixed the calculator

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.get('/', (req, res) => {
 
 app.post("/", (req, res) => {
 
-    let num1 = req.body.num1;
-    let num2 = req.body.num2;
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
 
-    let result = num1 + num2
+    let result = num1 + num2;
     res.send(`The answer is ${result}`);
 })
 
